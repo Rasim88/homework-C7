7a77ceaea2c8d19c8e42a3b1bbe197efb8ca19de
Rasim Fakhretdinov <111516877+Rasim88@users.noreply.github.com>
2022-09-15 20:58:15 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿// Задача №47
+
+Console.WriteLine("Enter number of rows: ");
+int linesVol = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number of columns: ");
+int columnsVol = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[linesVol, columnsVol];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+// Задача 50
+
+Console.WriteLine("Enter number of rows: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter numbers of columns: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int [,] SecondNum = new int [10,10];
+FillArrayRandomNumbers(SecondNum);
+
+if (n > SecondNum.GetLength(0) || m > SecondNum.GetLength(1))
+{
+    Console.WriteLine("There is no such element in");
+}
+else
+{
+    Console.WriteLine($"Element value {n} row и {m} column equally {SecondNum[n-1,m-1]}");
+}
+
+PrintArray(SecondNum);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-100, 100)/10;
+            }   
+        }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }   
+        Console.Write("]");
+        Console.WriteLine(""); 
+    }
+}
+
+// Задача 52
+
+Random random = new Random();
+int[,] arr = new int[random.Next(1, 10), random.Next(1, 10)];
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = random.Next(1, 10);
+        Console.Write(arr[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("----");
+Console.WriteLine(arr.GetLength(0));
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        sum += arr[i, j];
+    }
+    Console.Write($"{ sum / arr.GetLength(0)} ");
+}
+Console.ReadLine();
